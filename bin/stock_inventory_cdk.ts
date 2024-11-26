a5d317126b97884c411ac9843d25ad19f63e8329
Elias Adjetey <eliasadjetey@hotmail.com>
2024-11-26 20:49:03 +0000

    deploy serverless architecture with aws cdk

--- bin/stock_inventory_cdk.ts
+++ bin/stock_inventory_cdk.ts
@@ -1,6 +1,14 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { StockInventoryCdkStack } from '../lib/stock_inventory_cdk-stack';
+import * as dotenv from 'dotenv';
+dotenv.config()
 
 const app = new cdk.App();
-new StockInventoryCdkStack(app, 'StockInventoryCdkStack');
+
+new StockInventoryCdkStack(app, 'StockInventoryCdkStack', {
+    env: {
+        account: process.env.CDK_DEFAULT_ACCOUNT,
+        region: process.env.CDK_DEFAULT_REGION
+    }
+});
--- lib/stock_inventory_cdk-stack.ts
+++ lib/stock_inventory_cdk-stack.ts
@@ -1,19 +1,77 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { join } from 'path';
 
-export class StockInventoryCdkStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class StockInventoryCdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'StockInventoryCdkQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    /* PART 1 */
+    const inventoryBucket = new cdk.aws_s3.Bucket(this, 'Inventory-74621', {
+      bucketName: 'inventory-74621',
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
     });
 
-    const topic = new sns.Topic(this, 'StockInventoryCdkTopic');
+    const inventoryTable = new cdk.aws_dynamodb.Table(this, 'Inventory', {
+      tableName: 'Inventory',
+      partitionKey: { name: 'Store', type: cdk.aws_dynamodb.AttributeType.STRING },
+      stream: cdk.aws_dynamodb.StreamViewType.NEW_IMAGE,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    const loadInventoryFunction = new cdk.aws_lambda.Function(this, 'LoadInventoryFunction', {
+      functionName: 'LoadInventoryFunction',
+      runtime: cdk.aws_lambda.Runtime.PYTHON_3_9,
+      handler: 'loadInventory.lambda_handler',
+      code: cdk.aws_lambda.Code.fromAsset(join(__dirname, '..', 'lambda')),
+      initialPolicy: [
+        new cdk.aws_iam.PolicyStatement({
+          effect: cdk.aws_iam.Effect.ALLOW,
+          actions: ['dynamodb:*'],
+          resources: [inventoryTable.tableArn],
+        }),
+      ],
+    });
+
+    inventoryBucket.grantRead(loadInventoryFunction)
+
+    inventoryBucket.addEventNotification(
+      cdk.aws_s3.EventType.OBJECT_CREATED,
+      new cdk.aws_s3_notifications.LambdaDestination(loadInventoryFunction)
+    );
+
+    /* PART 2 */
+    const noStockTopic = new cdk.aws_sns.Topic(this, 'NoStock', {
+      topicName: 'NoStock',
+    });
+
+    const emailAddress = process.env.EMAIL_ADDRESS!
+    noStockTopic.addSubscription(new cdk.aws_sns_subscriptions.EmailSubscription(emailAddress));
+    
+    const checkInventoryFunction = new cdk.aws_lambda.Function(this, 'CheckInventoryFunction', {
+      functionName: 'CheckInventoryFunction',
+      runtime: cdk.aws_lambda.Runtime.PYTHON_3_9,
+      handler: 'checkInventory.lambda_handler',
+      code: cdk.aws_lambda.Code.fromAsset(join(__dirname, '..', 'lambda')),
+      environment: {
+        TOPIC_ARN: noStockTopic.topicArn,
+      },
+      initialPolicy: [
+        new cdk.aws_iam.PolicyStatement({
+          effect: cdk.aws_iam.Effect.ALLOW,
+          actions: ['sns:*'],
+          resources: [noStockTopic.topicArn],
+        }),
+      ],
+    });
+
+    inventoryTable.grantStreamRead(checkInventoryFunction);
+
+    checkInventoryFunction.addEventSourceMapping('InventoryStream', {
+      eventSourceArn: inventoryTable.tableStreamArn!,
+      startingPosition: cdk.aws_lambda.StartingPosition.LATEST,
+    });
   }
 }
